0fb580dbfc5ebe5cd05fec1fe7e02ae4b86cc5f7
Kave Mohammadi <kave.mdi@gmail.com>
2021-03-13 00:10:30 +0330

    add config.ts

---
+++ config.ts
@@ -0,0 +1,12 @@
+const config = {
+  fullName: "Kave Mohammadi",
+  nameOfThisRepo: "kave-me.github.io",
+  servedAt: "https://kave.me",
+
+  githubHandle: "kave-me",
+  twitterHandle: "kave_me",
+  linkedinHandle: "kavemohammadi",
+  emailAddress: "kave.mdi@gmail.com"
+}
+
+export default config;
