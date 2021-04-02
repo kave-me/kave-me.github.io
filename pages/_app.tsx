3570e954888d8546e74d85cd97d855a8f0faaaab
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-03 03:25:22 +0430

    add toggle theme according to global state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from 'src/apolloClient';
 import { ThemeProvider } from '@material-ui/styles';
-import theme from '../src/theme';
 import { useEffect } from 'react';
 import { CssBaseline } from '@material-ui/core';
-
-const darkTheme = true;
+import useTheme from '../src/globalState/useTheme';
 
 export default function App({ Component, pageProps }: AppProps) {
+  // TODO fix type declaration
+  const theme = useTheme((state) => state.theme) as any;
+
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -20,11 +21,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   const client = useApollo();
   return (
-    <ThemeProvider theme={darkTheme ? theme.dark : theme.light}>
-      <ApolloProvider client={client}>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
-      </ApolloProvider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
