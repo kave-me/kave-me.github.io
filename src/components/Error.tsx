b0eb24a15b84091d080244ecd6573edcd1ff9a2e
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-03 03:13:54 +0430

    add error page

---
+++ src/components/Error.tsx
@@ -0,0 +1,9 @@
+import GetHead from './utils/GetHead';
+export default function Error() {
+  return (
+    <>
+      <GetHead keyWord={'Error'} />
+      <h1>Please contact me at kave.mdi[at]gmail.com when you see this Error page!</h1>
+    </>
+  );
+}
