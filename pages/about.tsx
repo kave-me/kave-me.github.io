7f7a5fb32db9cacc8f4c094a0c039fda89d12276
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-07 03:59:43 +0430

    add about.tsx page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core';
+
+export default function About() {
+  return (
+    <>
+      <Typography variant={'h1'}>Hellooo</Typography>
+      <Typography variant={'body1'}>
+        I'm going to write a lengthy article about myself in this page, please check out this page
+        later 🤗
+      </Typography>
+    </>
+  );
+}
